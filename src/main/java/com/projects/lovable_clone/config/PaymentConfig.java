80424f44f5f903cb52cf1ad4c0343ec8999ef8d1
Lakshya-KP <lakshya.kp.25@gmail.com>
2026-05-01 15:47:46 +0530

    correction

--- src/main/java/com/projects/lovable_clone/config/PaymentConfig.java
+++ src/main/java/com/projects/lovable_clone/config/PaymentConfig.java
@@ -1,6 +1,6 @@
 package com.projects.lovable_clone.config;
 
-import com.stripe.Stripe
+import com.stripe.Stripe;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
