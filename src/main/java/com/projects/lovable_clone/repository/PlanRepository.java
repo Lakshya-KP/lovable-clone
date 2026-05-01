6e02560bbcfdb2cdace7037822423bfa6aef8ed5
Lakshya-KP <lakshya.kp.25@gmail.com>
2026-05-01 15:46:33 +0530

    Stripe Payments added

---
+++ src/main/java/com/projects/lovable_clone/repository/PlanRepository.java
@@ -0,0 +1,7 @@
+package com.projects.lovable_clone.repository;
+
+import com.projects.lovable_clone.entity.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
